1c8a114715a5f7479636dd7d8138d13f01ecd8dc
ddewaay <dondewaay@gmail.com>
2024-10-02 23:59:04 -0500

    Yert

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,10 +4,10 @@ const Footer = () => {
     return (
         <div className="footer">
             <motion.h2 whileHover={{scale: 1.1}}>
-                <a href="https://dondewaay.github.io/Portfolio/">Don DeWaay III</a>
+                <a href="https://dawndewaay.github.io/Portfolio/">Don DeWaay III</a>
             </motion.h2>
             <motion.h2 whileHover={{scale: 1.1}}>
-                <a href="https://github.com/dondewaay/To-Do">GitHub</a>
+                <a href="https://github.com/dawndewaay/To-Do">GitHub</a>
             </motion.h2>
         </div>
     )
